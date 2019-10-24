3f344858537723b43d5f9f0ae37629e790e49b7f
sylvia <hello@sylviacastro.co>
2019-10-23 21:21:16 -0500

    updated app.css

---
+++ build/precache-manifest.ee1eb834716f89d034cf6a6aa222716d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7ca186ea0be5c87ac8ef2d18c2c88a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "21cdfdc5bb21d8e09940",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "553fd04d1d7f361c15a5",
+    "url": "/robofriends/static/css/main.048af1a8.chunk.css"
+  },
+  {
+    "revision": "21cdfdc5bb21d8e09940",
+    "url": "/robofriends/static/js/2.dcda337d.chunk.js"
+  },
+  {
+    "revision": "553fd04d1d7f361c15a5",
+    "url": "/robofriends/static/js/main.8ebb22d3.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
